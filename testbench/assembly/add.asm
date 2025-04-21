5f6799b5289290179375464d311761686fa6f205
zakariamoknine <zakariamoknine3@gmail.com>
2025-04-21 22:14:51 +0100

    add comments for clarity

--- testbench/assembly/add.asm
+++ testbench/assembly/add.asm
@@ -1,10 +1,10 @@
-.data       ; begin .data section. .data section must exist and
+.data       ; begin .data section, this section must exist and
             ; come before the .text section
 
-a = 19      ; variable a assigned to 19
-            ; variables are to be 8-bit long, between 0 and 255
+a = 19      ; variable 'a' assigned to 19
+            ; variables are to be 8-bit long unsigned integers
 
-b = 43      ; variable b assigned to 43
+b = 43      ; variable 'b' assigned to 43
 
 .text       ; begin .text section
 
@@ -15,6 +15,6 @@ _start:     ; entry point must exist with the label _start
     ADD b   ; add the accumulator's content to the variable 'b'
             ; store the result in the accumulator register
 
-    OUT     ; show the accumulator's content in the display port
+    OUT     ; display the accumulator's content in the display port
 
     HLT     ; halt the CPU
--- testbench/assembly/sub.asm
+++ testbench/assembly/sub.asm
@@ -1,10 +1,10 @@
-.data       ; begin .data section. .data section must exist and
+.data       ; begin .data section, this section must exist and
             ; come before the .text section
 
-a = 215     ; variable a assigned to 215
-            ; variables are to be 8-bit long, between 0 and 255
+a = 215     ; variable 'a' assigned to 215
+            ; variables are to be 8-bit long unsigned integers
 
-b = 79      ; variable b assigned to 79
+b = 79      ; variable 'b' assigned to 79
 
 .text       ; begin .text section
 
@@ -15,6 +15,6 @@ _start:     ; entry point must exist with the label _start
     SUB b   ; subtract from the accumulator's content the variable 'b'
             ; store the result in the accumulator register
 
-    OUT     ; show the accumulator's content in the display port
+    OUT     ; display the accumulator's content in the display port
 
     HLT     ; halt the CPU
