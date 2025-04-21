7e8b0710bc0b397b71175b04af4ba3e0065757fd
zakariamoknine <zakariamoknine3@gmail.com>
2025-04-21 21:32:09 +0100

    more comments for clarifiction

--- testbench/assembly/add.asm
+++ testbench/assembly/add.asm
@@ -1,12 +1,20 @@
-.data
+.data       ; begin .data section. .data section must exist and
+            ; come before the .text section
 
-a = 19
-b = 43
+a = 19      ; variable a assigned to 19
+            ; variables are to be 8-bit long, between 0 and 255
 
-.text
+b = 43      ; variable b assigned to 43
 
-_start:
-    LDA a
-    ADD b
-    OUT
-    HLT
+.text       ; begin .text section
+
+_start:     ; entry point must exist with the label _start
+
+    LDA a   ; load the variable 'a' to the accumulator register
+
+    ADD b   ; add the accumulator's content to the variable 'b'
+            ; store the result in the accumulator register
+
+    OUT     ; show the accumulator's content in the display port
+
+    HLT     ; halt the CPU
--- testbench/assembly/sub.asm
+++ testbench/assembly/sub.asm
@@ -1,12 +1,20 @@
-.data
+.data       ; begin .data section. .data section must exist and
+            ; come before the .text section
 
-a = 215
-b = 79
+a = 215     ; variable a assigned to 215
+            ; variables are to be 8-bit long, between 0 and 255
 
-.text
+b = 79      ; variable b assigned to 79
 
-_start:
-    LDA a
-    SUB b
-    OUT
-    HLT 
+.text       ; begin .text section
+
+_start:     ; entry point must exist with the label _start
+
+    LDA a   ; load variable 'a' to the accumulator register
+
+    SUB b   ; subtract from the accumulator's content the variable 'b'
+            ; store the result in the accumulator register
+
+    OUT     ; show the accumulator's content in the display port
+
+    HLT     ; halt the CPU
